1f64860ab1265a35f276e9d3ebe2f38337cad2b6
qdequele <quentin@dequelen.me>
2017-10-16 13:09:16 +0200

    fix colleen

--- srcs/Colleen.c
+++ srcs/Colleen.c
@@ -3,6 +3,7 @@
 void lol() {}
 int main(void) {
 	// toto
-	char *c="#include <stdio.h>%c/* Hello */%cvoid lol() {}%cint main(void) {%c%c// toto%c%cchar *c=%c%s%c;%c%cprintf(c,10,10,10,10,9,10,9,34,c,34,10,9,10);%c}";
-	printf(c,10,10,10,10,9,10,9,34,c,34,10,9,10);
+	lol();
+	char *c="#include <stdio.h>%c/* Hello */%cvoid lol() {}%cint main(void) {%c%c// toto%c%clol();%c%cchar *c=%c%s%c;%c%cprintf(c,10,10,10,10,9,10,9,10,9,34,c,34,10,9,10);%c}";
+	printf(c,10,10,10,10,9,10,9,10,9,34,c,34,10,9,10);
 }
